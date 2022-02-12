a9a60255289d3e4f41ed72121e7561c0462b2411
osadezu <osadezu@gmail.com>
2022-02-12 17:06:55 -0500

    Setup django project and custom user model

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email',
+                              max_length=255, unique=True)
+    # avatar = models.ImageField(upload_to='user_avatar/', blank=True)
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = ['email']
+
+    def get_username(self):
+        return super().get_username()
